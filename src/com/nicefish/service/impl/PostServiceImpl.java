391abfa2ea1d668261e2b56425bd1e718b9e98f5
wsc_lord <1334996110@qq.com>
2017-02-15 12:20:22 +0800

    文章列表接口

--- src/com/nicefish/service/impl/PostServiceImpl.java
+++ src/com/nicefish/service/impl/PostServiceImpl.java
@@ -1,20 +1,37 @@
 package com.nicefish.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nicefish.dao.PostMapper;
+import com.nicefish.model.Post;
 import com.nicefish.service.PostService;
 
 @Service("postService")
 public class PostServiceImpl implements PostService {
+
+	@Autowired
 	private PostMapper postMapper;
 
-	public PostMapper getPostMapper() {
-		return postMapper;
+	@Override
+	public int insert(Post model) {
+		return postMapper.insertSelective(model);
 	}
-	@Autowired
-	public void setPostMapper(PostMapper postMapper) {
-		this.postMapper = postMapper;
+	
+	@Override
+	public Post findById(String id) {
+		return postMapper.selectByPrimaryKey(id);
+	}
+	
+	@Override
+	public List<Post> findAll() {
+		return postMapper.findAll();
+	}
+	
+	@Override
+	public int delete(String id) {
+		return postMapper.deleteByPrimaryKey(id);
 	}
 }
