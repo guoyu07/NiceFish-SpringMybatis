0521bfd566366403e19a607152c140467e719669
wsc_lord <1334996110@qq.com>
2017-02-15 16:33:01 +0800

    添加返回当前帖子评论信息列表

--- src/com/nicefish/controller/CommentController.java
+++ src/com/nicefish/controller/CommentController.java
@@ -1,14 +1,31 @@
 package com.nicefish.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nicefish.model.Comment;
 import com.nicefish.service.CommentService;
 
 @Controller
+@RequestMapping("/comments")
 public class CommentController {
+	
+	private final static ObjectMapper objectMapper = new ObjectMapper();
 	
 	@Autowired
 	private CommentService commentService;
 
+	@RequestMapping(value = "{postid}", method = RequestMethod.GET)
+	@ResponseBody
+	public String postFindKey(@PathVariable("postid")String postid) throws Exception {
+        List<Comment> list = commentService.findByPostId(postid);
+        return objectMapper.writeValueAsString(list);
+    }
 }
