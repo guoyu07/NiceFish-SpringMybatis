dc2888df6a64b1d5e5c259a84ff249c192aa26b1
wsc_lord <1334996110@qq.com>
2017-02-15 12:21:12 +0800

    新增dao层方法

--- src/com/nicefish/dao/PostMapper.java
+++ src/com/nicefish/dao/PostMapper.java
@@ -1,9 +1,11 @@
 package com.nicefish.dao;
 
+import java.util.List;
+
 import com.nicefish.model.Post;
 import com.nicefish.util.base.BaseMapper;
 
 public interface PostMapper extends BaseMapper<Post, String>{
     
-	
+	List<Post> findAll();
 }
