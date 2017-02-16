9e024b3fffe2b368e0ebdb586cec14ee3c1e83da
wsc_lord <1334996110@qq.com>
2017-02-16 17:51:52 +0800

    完善service接口与实现类

--- src/com/nicefish/service/UserService.java
+++ src/com/nicefish/service/UserService.java
@@ -1,6 +1,18 @@
 package com.nicefish.service;
 
+import java.util.List;
+
+import com.nicefish.model.User;
+
 
 public interface UserService {
 	
+	public int insert(User model);
+	
+	public User findById(String id);
+	
+	public List<User> findAll();
+	
+	public int delete(String id);
+
 }
--- src/com/nicefish/service/impl/UserServiceImpl.java
+++ src/com/nicefish/service/impl/UserServiceImpl.java
@@ -1,20 +1,42 @@
 package com.nicefish.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nicefish.dao.UserMapper;
+import com.nicefish.model.User;
 import com.nicefish.service.UserService;
 
 @Service("userService")
 public class UserServiceImpl implements UserService{
+	
+	@Autowired
 	private UserMapper userMapper;
 
-	public UserMapper getUserMapper() {
-		return userMapper;
+	
+	@Override
+	public int insert(User model) {
+		return userMapper.insertSelective(model);
 	}
-	@Autowired
-	public void setUserMapper(UserMapper userMapper) {
-		this.userMapper = userMapper;
+
+
+	@Override
+	public User findById(String id) {
+		return userMapper.selectByPrimaryKey(id);
 	}
+
+
+	@Override
+	public List<User> findAll() {
+		return userMapper.findAll();
+	}
+
+
+	@Override
+	public int delete(String id) {
+		return userMapper.deleteByPrimaryKey(id);
+	}
+	
 }
