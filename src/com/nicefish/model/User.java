f22fb17c86dfeb8a261d3e01fea9ea930338f699
zhang.xiaofei <damoqiongqiu@126.com>
2017-03-01 23:00:29 +0800

    建个简单的实体类，今天就到这里吧，睡觉了；

--- src/com/nicefish/model/User.java
+++ src/com/nicefish/model/User.java
@@ -1,5 +1,54 @@
 package com.nicefish.model;
 
 public class User {
-
+	private String userId;
+	private String userName;
+	private String password;
+	private String nickeName;
+	private String realName;//真实姓名
+	private String eName;//英文名
+	private int status;
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getNickeName() {
+		return nickeName;
+	}
+	public void setNickeName(String nickeName) {
+		this.nickeName = nickeName;
+	}
+	public String getRealName() {
+		return realName;
+	}
+	public void setRealName(String realName) {
+		this.realName = realName;
+	}
+	public String geteName() {
+		return eName;
+	}
+	public void seteName(String eName) {
+		this.eName = eName;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
 }
