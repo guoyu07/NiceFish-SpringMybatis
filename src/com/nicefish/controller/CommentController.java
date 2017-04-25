50ab9c3daf5eadb25e8e65ee49b75002372e0e51
zhang.xiaofei <damoqiongqiu@126.com>
2017-04-25 17:57:10 +0800

    完成comment部分的重构；

--- src/com/nicefish/controller/CommentController.java
+++ src/com/nicefish/controller/CommentController.java
@@ -5,11 +5,11 @@ import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,20 +24,27 @@ public class CommentController extends BaseController {
 	@Autowired
 	private CommentService commentService;
 
-	@RequestMapping(value = "/{postid}", method = RequestMethod.GET)
+	@RequestMapping(value = "/{postId}", method = RequestMethod.GET)
 	@ResponseBody
-	public String PostFindByPid(@PathVariable("postid") String postid)
+	public String PostFindByPid(@PathVariable("postId") String postId)
 			throws Exception {
-		List<POComment> list = commentService.getCommentListByPostId(postid);
+		List<POComment> list = commentService.getCommentListByPostId(postId);
 		return this.writeJSON("comments", list);
 	}
 
 	@RequestMapping(value = "/newComment", method = RequestMethod.POST)
 	@ResponseBody
-	public Map<String, Object> newComment(VONewComment voNewComment,
-			HttpServletRequest request, HttpSession session)
-			throws IllegalAccessException, InvocationTargetException {
+	public Map<String, Object> newComment(@RequestBody VONewComment voNewComment,
+			HttpServletRequest request) throws IllegalAccessException,
+			InvocationTargetException {
 		commentService.newComment(voNewComment);
+		return this.ajaxSuccessResponse();
+	}
+
+	@RequestMapping(value = "/delComment/{commentId}", method = RequestMethod.GET)
+	@ResponseBody
+	public Map<String, Object> delComment(@PathVariable("commentId") String commentId) {
+		commentService.delCommentById(commentId);
 		return this.ajaxSuccessResponse();
 	}
 }
