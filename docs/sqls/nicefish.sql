04c11a88ce1dd5f1ef1199b58d2b62fea8cb975c
zhang.xiaofei <damoqiongqiu@126.com>
2017-05-04 11:51:18 +0800

    更新项目设置和sql语句；

--- docs/sqls/nicefish.sql
+++ docs/sqls/nicefish.sql
@@ -40,6 +40,10 @@ CREATE TABLE `comment` (
   KEY `index_postid_status_time` (`COMMENT_TIME`,`POST_ID`,`STATUS`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+/*Data for the table `comment` */
+
+insert  into `comment`(`COMMENT_ID`,`P_ID`,`USER_ID`,`USER_NAME`,`NICK_NAME`,`CONTENT`,`COMMENT_IP`,`COMMENT_TIME`,`POST_ID`,`STATUS`) values ('03edabab-ea4f-4c8f-a807-21ef00680eae','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:42','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('07d1c268-98da-455c-aa2f-264d74aa9079','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:41','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('0d643d9a-ecbd-4ac9-9ccc-de8380ca182a','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:40','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('10a992e2-f59a-4566-96bd-6864d0b81085','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:41','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('19119904-80fd-4027-ac9d-6c7bfb23aeb4','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:41','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('1e55ca2f-d197-4414-8907-91cb18c12dec','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:42','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('1f1b5745-88eb-42d1-a184-729cf32a6aa7','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:39','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('247ce83a-818c-4848-8c9f-544777eae539','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:43','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('38737610-3061-4dcc-ab6d-47207902588e','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:42','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('4527354c-5753-453e-bbed-698e59f0547a','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:42','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('45781060-cada-40fa-8ba7-010f9b3b5a90','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:42','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('4c1b3a21-f7c9-483e-aecf-0e6897b59181','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:43','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('4d42c058-549f-4e0b-aca2-f19f921001d5','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:43','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('533a605e-69ab-49ca-aafc-3f4a9de38556','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:40','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('537afab1-92a6-4f80-b1bc-adcc26927aed','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:39','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('5a2e8630-37eb-4e12-b00d-442b7911e6f8','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:42:32','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('5c1f6122-b1f0-4bab-8221-c4d1e60064d1','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:41','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('69def43b-f601-49ee-af2d-6aae19fc6d39','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:42:32','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('6c8f08da-6754-41be-bd5b-13058efb21f6','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:42:32','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('7e88179b-3f2d-4720-8512-af282e1f4254','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:39','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('80107385-de18-4d78-a52a-15dd23653248','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:40','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('811d245d-76d0-4c5a-97ff-6e06dea14240','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:39','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('8a75e90d-ef61-4f79-89a6-aa5869c480b9','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:42','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('92621bbf-26fd-4dc1-8ea3-36b8e386b988','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:40','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('92860cad-8587-4ad9-98fa-d6fd03d6de87','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:40','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('98dc0739-06ff-45a1-bea3-2cb8c338b4de','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:42:32','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('a00a6b87-6173-45c5-be22-acd2acc62fce','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:42:33','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('a1afade8-ea27-44a3-b250-569e7ce175cd','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:40','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('a39026d3-3e48-416f-a7fa-9cdc849a8d9b','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:39','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('b2d2349c-c65e-4dfa-ae10-f12bff95a9b1','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:40','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('b4eb4e0a-dd53-4e69-a82b-cbd532541b30','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:43','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('b87d067d-c570-4da1-b7e1-8a67dc3123d2','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:42','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('c2e1f4db-2f1d-4f2e-95bb-a38d5aca0261','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:38','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('cdc9e4ff-fa8f-4586-81f4-d78fbf8c70db','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:41','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('e1ceeb55-abda-4c52-bf2d-70aac28046dd','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:42:32','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('e293b381-1f17-4fd9-a835-6c4d6d7e056b','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:39','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1),('e70569cb-bfbc-4700-9414-526b63a011e7','-1','ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋','这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。这里可以提交评论。',NULL,'2017-05-03 22:31:41','78dd5fd1-7b59-4135-9bb2-1cb0057b690a',1);
+
 /*Table structure for table `permission` */
 
 DROP TABLE IF EXISTS `permission`;
@@ -50,6 +54,8 @@ CREATE TABLE `permission` (
   `PERMISSION_DESC` varchar(512) NOT NULL,
   PRIMARY KEY (`PERMISSION_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='权限';
+
+/*Data for the table `permission` */
 
 /*Table structure for table `post` */
 
@@ -77,6 +83,10 @@ CREATE TABLE `post` (
   KEY `index_status` (`STATUS`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+/*Data for the table `post` */
+
+insert  into `post`(`POST_ID`,`POST_TITLE`,`POST_TIME`,`POST_CONTENT`,`ORIGINAL_URL`,`POST_TYPE`,`LAST_MODIFY_TIME`,`READ_TIMES`,`LIKED_TIMES`,`COMMENT_TIMES`,`USER_ID`,`USER_NAME`,`NICK_NAME`,`ENABLE_COMMENT`,`STATUS`) values ('78dd5fd1-7b59-4135-9bb2-1cb0057b690a','你是一个疯子','2017-05-03 22:10:39','<p>你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子你是一个疯子</p>',NULL,0,NULL,1,0,0,'ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'大漠穷秋',1,4);
+
 /*Table structure for table `role` */
 
 DROP TABLE IF EXISTS `role`;
@@ -89,6 +99,8 @@ CREATE TABLE `role` (
   PRIMARY KEY (`ROLE_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色';
 
+/*Data for the table `role` */
+
 /*Table structure for table `role_permission` */
 
 DROP TABLE IF EXISTS `role_permission`;
@@ -100,6 +112,8 @@ CREATE TABLE `role_permission` (
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色和权限映射';
 
+/*Data for the table `role_permission` */
+
 /*Table structure for table `sys_params` */
 
 DROP TABLE IF EXISTS `sys_params`;
@@ -110,6 +124,10 @@ CREATE TABLE `sys_params` (
   `PARAM_DESC` varchar(512) DEFAULT NULL,
   PRIMARY KEY (`PARAM_KEY`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统参数表。';
+
+/*Data for the table `sys_params` */
+
+insert  into `sys_params`(`PARAM_KEY`,`PARAM_VALUE`,`PARAM_DESC`) values ('COMMENT_PAGE_NUM','10','文章评论每页显示的条数'),('COMMENT_TIME_INTERVAL','3','*分钟内不允许多次发表评论'),('HEAD_IMG_FILE_SIZE_MAX','1','用户头像最大尺寸（M）'),('MAX_COMMENT_SIZE','10000','评论Bean缓存容量'),('MAX_FILE_SIZE','10','写文章上传附件时文件的最大尺寸（M）'),('POST_COMMENT_NUM','20','文章评论每页显示的条数'),('POST_CONTENT_LEN','140','文章列表显示的缩略内容字符数'),('POST_FILE_SIZE_MAX','50','文章附件单个文件最大尺寸（M）'),('POST_IMG_FILE_SIZE_MAX','1','文章内部图片最大文件尺寸（M）'),('POST_PAGE_NUM','20','文章列表每页显示文章的数量'),('UPLOAD_FILE_DIR','TOMCAT','上传文件存放的目录');
 
 /*Table structure for table `upload` */
 
@@ -129,6 +147,8 @@ CREATE TABLE `upload` (
   `FILE_DESC` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`UP_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='上传的文件，会员头像、用户头像、门店图片介绍、项目图片介绍等，所有上传的文件都记录在这张表。';
+
+/*Data for the table `upload` */
 
 /*Table structure for table `user` */
 
@@ -153,6 +173,10 @@ CREATE TABLE `user` (
   PRIMARY KEY (`USER_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户\n预计百万级的数据';
 
+/*Data for the table `user` */
+
+insert  into `user`(`USER_ID`,`USER_NAME`,`PASSWORD`,`EMAIL`,`REAL_NAME`,`NICK_NAME`,`E_NAME`,`QQ`,`WEIXIN`,`CELL_PHONE`,`USER_DESC`,`UP_ID`,`STATUS`,`REG_TIME`,`LASTLOGIN_TIME`) values ('ced14f57-5fec-40f6-b0b7-abe77a5d9f21',NULL,'57d6c2aa11b6a0f842e4b1297b79e906','damoqiongqiu@126.com',NULL,'大漠穷秋',NULL,NULL,NULL,NULL,NULL,NULL,1,'2017-05-03 22:10:22','0000-00-00 00:00:00');
+
 /*Table structure for table `user_role` */
 
 DROP TABLE IF EXISTS `user_role`;
@@ -163,6 +187,8 @@ CREATE TABLE `user_role` (
   `ROLE_ID` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户角色映射';
+
+/*Data for the table `user_role` */
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
