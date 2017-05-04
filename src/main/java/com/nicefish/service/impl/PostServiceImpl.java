74677f00ef363eb151b7a92add76b9690dedcc76
zhongzhong0505 <zt_zhong@qq.com>
2017-05-04 21:23:02 +0800

    可以正常启动了

--- src/main/java/com/nicefish/service/impl/PostServiceImpl.java
+++ src/main/java/com/nicefish/service/impl/PostServiceImpl.java
@@ -1,15 +1,5 @@
 package com.nicefish.service.impl;
 
-import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
-
-import org.apache.commons.beanutils.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.nicefish.dao.POPostMapper;
 import com.nicefish.po.POPost;
 import com.nicefish.po.POSysParam;
@@ -19,6 +9,16 @@ import com.nicefish.service.SysParamService;
 import com.nicefish.service.UserService;
 import com.nicefish.utils.WebUtil;
 import com.nicefish.vo.VONewPost;
+import org.apache.commons.beanutils.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @Service("postService")
 public class PostServiceImpl implements PostService {
@@ -26,22 +26,19 @@ public class PostServiceImpl implements PostService {
 	private SysParamService sysParamService;
 	
 	@Autowired
-	private UserService userSerivce;
+	private UserService userService;
 	
-	@Autowired
+	@Resource
 	private POPostMapper postMapper;
 
-	@Override
 	public POPost getPostById(String id) {
 		return postMapper.selectByPrimaryKey(id);
 	}
 	
-	@Override
 	public List<POPost> getAllPosts() {
 		return postMapper.findAll();
 	}
 	
-	@Override
 	public List<POPost> getPostListByPage(String currentPage) {
 		POSysParam poSysParam=sysParamService.findByParamKey("POST_PAGE_NUM");
 		String pageSize=poSysParam.getParamValue();
@@ -53,12 +50,11 @@ public class PostServiceImpl implements PostService {
 		return postMapper.selectByPage(start,limit);
 	}
 	
-	@Override
 	public String newPost(VONewPost voNewPost) throws IllegalAccessException, InvocationTargetException {
 		POPost poPost=new POPost();
 		BeanUtils.copyProperties(poPost, voNewPost);
 		
-		POUser poUser=userSerivce.findById(voNewPost.getUserId());
+		POUser poUser=userService.findById(voNewPost.getUserId());
 		poPost.setUserId(poUser.getUserId());
 		poPost.setUserName(poUser.getUserName());
 		poPost.setNickName(poUser.getNickName());
@@ -69,12 +65,10 @@ public class PostServiceImpl implements PostService {
 		return postId;
 	}
 	
-	@Override
 	public int deleteById(String postId) {
 		return postMapper.deleteByPrimaryKey(postId);
 	}
 
-	@Override
 	public List<POPost> getPostByTitle(String key) {
 		Map<String,Object> map=new HashMap<String,Object>();
 		if(key!=null&&!key.equals(null)){
@@ -83,7 +77,6 @@ public class PostServiceImpl implements PostService {
 		return postMapper.findByTitle(map);
 	}
 
-	@Override
 	public String getTotalPages() {
 		int totalCount=postMapper.selectCount();
 		POSysParam poSysParam=sysParamService.findByParamKey("POST_PAGE_NUM");
@@ -91,13 +84,11 @@ public class PostServiceImpl implements PostService {
 		return WebUtil.calcPages(totalCount+"", pageSize);
 	}
 
-	@Override
 	public String getTotalItemsNum() {
 		int totalCount=postMapper.selectCount();
 		return totalCount+"";
 	}
 	
-	@Override
 	public Map<String,Object> getPagerParam() {
 		int totalCount=postMapper.selectCount();
 		
