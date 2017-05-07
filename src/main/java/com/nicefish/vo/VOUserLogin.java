b2d38cf5130fc6e70a0ca4090abeedd9d3f50da4
zhongzhong0505 <zt_zhong@qq.com>
2017-05-07 20:43:52 +0800

    提交一把

--- src/main/java/com/nicefish/vo/VOUserLogin.java
+++ src/main/java/com/nicefish/vo/VOUserLogin.java
@@ -3,20 +3,21 @@ package com.nicefish.vo;
 public class VOUserLogin {
 	private String userName;
 	private String email;
-	private String password;
+	private String nickName;
 	private boolean rememberMe;
+
+	public String getNickName(){
+		return this.nickName;
+	}
+	public void setNickName(String nickName){
+		this.nickName = nickName;
+	}
 	
 	public String getUserName() {
 		return userName;
 	}
 	public void setUserName(String userName) {
 		this.userName = userName;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
 	}
 	public boolean isRememberMe() {
 		return rememberMe;
