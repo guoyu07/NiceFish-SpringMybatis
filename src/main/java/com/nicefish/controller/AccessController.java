aef0e559c1e3d22b4ca3ca85bdf5735598fd6497
zhongzhong0505 <zt_zhong@qq.com>
2017-05-18 11:19:35 +0800

    sdfdsfds

--- src/main/java/com/nicefish/controller/AccessController.java
+++ src/main/java/com/nicefish/controller/AccessController.java
@@ -39,7 +39,7 @@ public class AccessController extends BaseController {
         this.userService.ensureUser(user);
         SecurityUtils.getSubject().login(new UsernamePasswordToken(user.getUserName(),user.getPassword()));
 
-        userLogin.setToken(TokenUtils.generate(user.getUserId(),"id",30*60*1000));
+        userLogin.setToken(TokenUtils.generate("id",user.getUserId(),"issuer",30*60*1000));
         BeanUtils.copyProperties(userLogin, user);
         return userLogin;
 
